8b536549fc7495bd674138de29a23421a977dc9d
Андрей Астафьев <a.astafev@bars-open.ru>
2018-05-03 12:47:43 +0300

    event emitter

--- lesson_3/eventemitter_example_2.js
+++ lesson_3/eventemitter_example_2.js
@@ -1,6 +1,6 @@
-let util = require('util');
-let eventEmitter = require('events');
-const FS = require('fs');
+const util = require('util');
+const eventEmitter = require('events');
+const fs = require('fs');
 const FILE_PARAM = {
     'flags': 'a',
     'encoding': 'utf-8',
@@ -10,19 +10,31 @@ const FILE_PARAM = {
 class InputAction extends eventEmitter.EventEmitter{   
     
     constructor(name, file){
+        super();
         this.name = name;
         this.file = file;
-        this.writeStream = FS.createWriteStream('./'+file+'.txt', FILE_PARAM);
+        this.writeStream = fs.createWriteStream(
+            './'+name+'.txt', FILE_PARAM
+        );
+    }
+    
+    isBuffer(data){
+        return data instanceof Buffer;
     }
     
     action(input){
+        if (this.isBuffer(input))
+            input = input.toString();
+        
         let command = input.substr(0, 3);
         let str = input.substr(3, input.length);
         
         switch(command){
             case 'wr:':
+                this.emit('write', str, this.writeStream);
                 break;
             case 'en:':
+                this.emit('end', str, this.writeStream);
                 break;
             default:
                 process.stdout.write('Undefined command. Try Again!');
@@ -32,14 +44,13 @@ class InputAction extends eventEmitter.EventEmitter{
 }
 
 let obj = new InputAction('somename','test');
-//util.inherits(obj, eventEmitter);
 
-obj.on('write',(data)=>{
-    this.writeStream(data, 'utf-8');
+obj.on('write',(data, writeStream)=>{
+    writeStream.write(data, 'utf-8');
 });
 
-obj.on('end',(data)=>{
-    this.writeStream(data, 'utf-8');
+obj.on('end',(data, writeStream)=>{
+    writeStream.write(data, 'utf-8');
     process.exit();
 });
 

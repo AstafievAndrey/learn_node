d94fcde25cbe6475f37eab027f66eb88279fae3e
Андрей Астафьев <a.astafev@bars-open.ru>
2018-06-14 09:05:00 +0300

    работа с файловой системой

--- lesson_6_fs/example_1.js
+++ lesson_6_fs/example_1.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const util = require('util');
+const Mode = require('stat-mode');
 
 fs.stat('./kotik.jpg',(err, stat)=>{
     if(err) return console.log(err);
+    let mode = new Mode(stat);
     console.log(util.inspect(stat));
+    console.log(mode.toString());
+    console.log(`Group execute ${mode.group.execute}`);
+    console.log(`Other write ${mode.others.write}`);
+    console.log(`Owner read ${mode.owner.read}`);
+    console.log(`Owner write ${mode.owner.write}`);
 });
